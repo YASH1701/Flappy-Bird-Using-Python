4f707b79dd4056705f87b32d0e843b009102a119
YASH <yashpandhare9870@gmail.com>
2020-10-02 22:04:06 +0530

    Update flappy.py

--- flappy.py
+++ flappy.py
@@ -4,9 +4,9 @@ import pygame
 from pygame.locals import * 
 
 
-FPS = 32
-SCREENWIDTH = 289
-SCREENHEIGHT = 511
+FPS = 60
+SCREENWIDTH = 1080
+SCREENHEIGHT = 1920
 SCREEN = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
 GROUNDY = SCREENHEIGHT * 0.8
 GAME_SPRITES = {}
